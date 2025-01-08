2ad1dd894c2e60b6e0f3683a7bd1d482df367137
diegostafa <dstafa.dev@gmail.com>
2025-01-09 00:53:17 +0100

    reserve space for sort markers

--- src/stateful_table.rs
+++ src/stateful_table.rs
@@ -92,78 +92,9 @@ pub struct StatefulTable<'a, T: Tabular> {
     indexed: bool,
 }
 impl<'a, T: Tabular> StatefulTable<'a, T> {
-    fn build_header(
-        alignments: &[Alignment],
-        selected_col: Option<usize>,
-        selected_col_ord: Ordering,
-        header_style: Style,
-    ) -> Option<Row<'a>> {
-        T::column_names().map(|header| {
-            Row::new(
-                header
-                    .into_iter()
-                    .zip(alignments)
-                    .enumerate()
-                    .map(|(i, (c, a))| {
-                        Text::raw(match selected_col {
-                            Some(col) if col == i => Self::format_column_name(c, selected_col_ord),
-                            _ => c,
-                        })
-                        .alignment(*a)
-                    }),
-            )
-            .style(header_style)
-        })
-    }
-    fn format_column_name(s: String, ord: Ordering) -> String {
-        match ord {
-            Ordering::Less => format!("{s}▲"),
-            Ordering::Equal => format!("{s}-"),
-            Ordering::Greater => format!("{s}▼"),
-        }
-    }
-
-    fn sort_rows(&mut self) {
-        if let Some(col) = self.selected_col() {
-            if self.indexed && col == 0 {
-                return;
-            }
-            let alignments = Self::alignemnts();
-            let mut data = self
-                .data
-                .clone()
-                .into_iter()
-                .zip(std::mem::take(&mut self.values))
-                .collect_vec();
-
-            match self.selected_col_ord {
-                Ordering::Less => data.sort_by(|a, b| a.0.cmp_by_col(&b.0, col)),
-                Ordering::Greater => data.sort_by(|a, b| b.0.cmp_by_col(&a.0, col)),
-                Ordering::Equal => {}
-            }
-
-            let (data, values): (Vec<_>, Vec<_>) = data.into_iter().unzip();
-            let rows = if self.indexed {
-                // rebuild indexes
-                let dedup = data.iter().map(T::data).collect();
-                Self::build_rows(&IndexedRow::from(dedup), &alignments)
-            } else {
-                Self::build_rows(&data, &alignments)
-            };
-            let mut table = std::mem::take(&mut self.table);
-            table = table.rows(rows);
-            if let Some(header) = Self::build_header(
-                &alignments,
-                self.selected_col(),
-                self.selected_col_ord,
-                self.style.header,
-            ) {
-                table = table.header(header);
-            }
-            self.table = table;
-            self.values = values;
-        }
-    }
+    const SORT_ASC: char = '▲';
+    const SORT_NONE: char = '-';
+    const SORT_DESC: char = '▼';
 
     pub fn new(
         data: Vec<T>,
@@ -180,71 +111,6 @@ impl<'a, T: Tabular> StatefulTable<'a, T> {
         title: Option<String>,
     ) -> StatefulTable<'a, IndexedRow<T>> {
         StatefulTable::build_table(IndexedRow::from(data), state, style, title, true)
-    }
-
-    fn build_table(
-        data: Vec<T>,
-        mut state: TableState,
-        mut style: TableStyle<'a>,
-        title: Option<String>,
-        indexed: bool,
-    ) -> Self {
-        let values = data.iter().map(T::value).collect();
-        if let Some(idx) = state.selected() {
-            state.select(Some(idx.clamp(0, data.len().saturating_sub(1))));
-        }
-
-        let col_widths = Self::columns_max_widths(&data);
-        let constraints = col_widths
-            .iter()
-            .zip(T::column_constraints().iter())
-            .map(|(s, c)| c(*s))
-            .collect_vec();
-
-        let col_constraints = constraints
-            .clone()
-            .into_iter()
-            .interleave(vec![
-                Constraint::Length(style.column_spacing);
-                constraints.len() - 1
-            ])
-            .collect();
-
-        let alignments = Self::alignemnts();
-        let mut table = Table::new(Self::build_rows(&data, &alignments), constraints)
-            .style(style.normal)
-            .column_spacing(style.column_spacing)
-            .row_highlight_style(style.highlight)
-            .column_highlight_style(style.col_highlight);
-
-        let mut padding = Padding::default();
-        if let Some(header) = Self::build_header(&alignments, None, Ordering::Equal, style.header) {
-            padding.t += 1;
-            table = table.header(header);
-        }
-
-        padding.add_padding(style.block.1);
-        if let Some(title) = &title {
-            style.block.0 = style.block.0.title(title.clone());
-        }
-        table = table.block(style.block.0.clone());
-        let inner_width =
-            col_widths.iter().sum::<u16>() + (style.column_spacing * (col_widths.len() - 1) as u16);
-
-        Self {
-            table,
-            state,
-            style,
-            padding,
-            inner_width,
-            values,
-            data,
-            col_constraints,
-            area: Rect::default(),
-            keymap: KeyMap::default(),
-            selected_col_ord: Ordering::Equal,
-            indexed,
-        }
     }
     pub fn selected_value(&self) -> Option<&T::Value> {
         self.state.selected().and_then(|i| self.values.get(i))
@@ -381,35 +247,6 @@ impl<'a, T: Tabular> StatefulTable<'a, T> {
                 .map(|(i, _)| i / 2)
         })
     }
-    fn columns_max_widths(data: &[T]) -> Vec<u16> {
-        let mut data = data.iter().map(T::content).collect_vec();
-        if let Some(headers) = T::column_names() {
-            data.push(headers);
-        }
-        if data.is_empty() {
-            return vec![];
-        }
-        let widths = |a: Vec<String>| a.iter().map(|e| e.len() as u16).collect();
-        let max_widths = |a: Vec<u16>, b: Vec<u16>| (0..a.len()).map(|i| a[i].max(b[i])).collect();
-        data.into_iter().map(widths).reduce(max_widths).unwrap()
-    }
-    fn build_rows(data: &[impl Tabular], alignments: &[Alignment]) -> Vec<Row<'a>> {
-        data.iter()
-            .map(|row| {
-                Row::new(
-                    row.content()
-                        .into_iter()
-                        .zip(alignments)
-                        .map(|(c, a)| Text::raw(c).alignment(*a)),
-                )
-                .style(row.style())
-                .height(T::row_height())
-            })
-            .collect()
-    }
-    fn alignemnts() -> Vec<Alignment> {
-        T::column_alignments().unwrap_or(vec![Alignment::default(); T::column_constraints().len()])
-    }
 
     pub fn select_next(&mut self) {
         self.select_relative(1);
@@ -470,6 +307,170 @@ impl<'a, T: Tabular> StatefulTable<'a, T> {
             None => self.selected_col_ord = Ordering::Equal,
         }
         self.state.select_column(Some(idx));
+    }
+
+    // --- helpers
+    fn build_table(
+        data: Vec<T>,
+        mut state: TableState,
+        mut style: TableStyle<'a>,
+        title: Option<String>,
+        indexed: bool,
+    ) -> Self {
+        let values = data.iter().map(T::value).collect();
+        if let Some(idx) = state.selected() {
+            state.select(Some(idx.clamp(0, data.len().saturating_sub(1))));
+        }
+
+        let col_widths = Self::columns_max_widths(&data);
+        let constraints = col_widths
+            .iter()
+            .zip(T::column_constraints().iter())
+            .map(|(s, c)| c(*s))
+            .collect_vec();
+
+        let col_constraints = constraints
+            .clone()
+            .into_iter()
+            .interleave(vec![
+                Constraint::Length(style.column_spacing);
+                constraints.len() - 1
+            ])
+            .collect();
+
+        let alignments = Self::alignemnts();
+        let mut table = Table::new(Self::build_rows(&data, &alignments), constraints)
+            .style(style.normal)
+            .column_spacing(style.column_spacing)
+            .row_highlight_style(style.highlight)
+            .column_highlight_style(style.col_highlight);
+
+        let mut padding = Padding::default();
+        if let Some(header) = Self::build_header(&alignments, None, Ordering::Equal, style.header) {
+            padding.t += 1;
+            table = table.header(header);
+        }
+
+        padding.add_padding(style.block.1);
+        if let Some(title) = &title {
+            style.block.0 = style.block.0.title(title.clone());
+        }
+        table = table.block(style.block.0.clone());
+        let inner_width =
+            col_widths.iter().sum::<u16>() + (style.column_spacing * (col_widths.len() - 1) as u16);
+
+        Self {
+            table,
+            state,
+            style,
+            padding,
+            inner_width,
+            values,
+            data,
+            col_constraints,
+            area: Rect::default(),
+            keymap: KeyMap::default(),
+            selected_col_ord: Ordering::Equal,
+            indexed,
+        }
+    }
+    fn columns_max_widths(data: &[T]) -> Vec<u16> {
+        let widths = |a: Vec<String>| a.iter().map(|e| e.len() as u16).collect();
+        let header_widths = |a: Vec<String>| a.iter().map(|e| 1 + e.len() as u16).collect();
+        let max_widths = |a: Vec<u16>, b: Vec<u16>| (0..a.len()).map(|i| a[i].max(b[i])).collect();
+        let mut row_widths = data.iter().map(T::content).map(widths).collect_vec();
+        if let Some(header_widths) = T::column_names().map(header_widths) {
+            row_widths.push(header_widths);
+        }
+        row_widths.into_iter().reduce(max_widths).unwrap()
+    }
+    fn build_rows(data: &[impl Tabular], alignments: &[Alignment]) -> Vec<Row<'a>> {
+        data.iter()
+            .map(|row| {
+                Row::new(
+                    row.content()
+                        .into_iter()
+                        .zip(alignments)
+                        .map(|(c, a)| Text::raw(c).alignment(*a)),
+                )
+                .style(row.style())
+                .height(T::row_height())
+            })
+            .collect()
+    }
+    fn alignemnts() -> Vec<Alignment> {
+        T::column_alignments().unwrap_or(vec![Alignment::default(); T::column_constraints().len()])
+    }
+    fn build_header(
+        alignments: &[Alignment],
+        selected_col: Option<usize>,
+        selected_col_ord: Ordering,
+        header_style: Style,
+    ) -> Option<Row<'a>> {
+        T::column_names().map(|header| {
+            Row::new(
+                header
+                    .into_iter()
+                    .zip(alignments)
+                    .enumerate()
+                    .map(|(i, (c, a))| {
+                        Text::raw(match selected_col {
+                            Some(col) if col == i => match selected_col_ord {
+                                Ordering::Less => format!("{c}{}", Self::SORT_ASC),
+                                Ordering::Equal => format!("{c}{}", Self::SORT_NONE),
+                                Ordering::Greater => format!("{c}{}", Self::SORT_DESC),
+                            },
+                            _ => c,
+                        })
+                        .alignment(*a)
+                    }),
+            )
+            .style(header_style)
+        })
+    }
+    fn sort_rows(&mut self) {
+        if let Some(col) = self.selected_col() {
+            if self.indexed && col == 0 {
+                return;
+            }
+            let mut data = self
+                .data
+                .clone()
+                .into_iter()
+                .zip(std::mem::take(&mut self.values))
+                .collect_vec();
+
+            match self.selected_col_ord {
+                Ordering::Less => data.sort_by(|a, b| a.0.cmp_by_col(&b.0, col)),
+                Ordering::Greater => data.sort_by(|a, b| b.0.cmp_by_col(&a.0, col)),
+                Ordering::Equal => {}
+            }
+
+            let (data, values): (Vec<_>, Vec<_>) = data.into_iter().unzip();
+            let alignments = Self::alignemnts();
+            let rows = if self.indexed {
+                // rebuild indexes
+                Self::build_rows(
+                    &IndexedRow::from(data.iter().map(T::data).collect()),
+                    &alignments,
+                )
+            } else {
+                Self::build_rows(&data, &alignments)
+            };
+
+            let mut table = std::mem::take(&mut self.table);
+            table = table.rows(rows);
+            if let Some(header) = Self::build_header(
+                &alignments,
+                self.selected_col(),
+                self.selected_col_ord,
+                self.style.header,
+            ) {
+                table = table.header(header);
+            }
+            self.table = table;
+            self.values = values;
+        }
     }
 }
 impl<T: Tabular> StatefulWidget for StatefulTable<'_, T> {
